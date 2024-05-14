43b1bc46b786e3a993d83c12e4cafda36358619d
--global <--global>
2024-05-15 01:10:40 +0300

    no finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,61 @@
 import React, { memo } from "react";
+import logo from "@/assets/images/logo.png";
+import Image from "next/image";
+import img1 from "@/assets/images/footer-1.png";
+import img2 from "@/assets/images/footer-2.png";
+import img3 from "@/assets/images/footer-3.png";
+import img4 from "@/assets/images/footer-4.png";
+import "./footer.css";
 
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__info">
+          <div className="footer__left">
+            <Image src={logo} alt="logo" className="footer__img" />
+            <p className="footer__text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+          <div className="footer__card">
+            <h2 className="footer__title">Contact us</h2>
+            <p className="footer__text">
+              E: info@example.com <br /> P: +94 7670000000 <br /> A: 123
+              Hospital rd, <br />
+              Kalubowila, Dehiwela
+            </p>
+          </div>
+          <div className="footer__card">
+            <h2 className="footer__title">Useful links</h2>
+            <p className="footer__text">Shop All</p>
+            <p className="footer__text">Tempered Glass</p>
+            <p className="footer__text">Back-cover</p>
+            <p className="footer__text">About Us</p>
+          </div>
+          <div className="footer__right">
+            <div className="footer__box">
+              <Image src={img1} alt="logo" className="footer__icon" />
+              <p className="footer__text">Whatsapp</p>
+            </div>
+            <div className="footer__box">
+              <Image src={img2} alt="logo" className="footer__icon" />
+              <p className="footer__text">Facebook</p>
+            </div>
+            <div className="footer__box">
+              <Image src={img3} alt="logo" className="footer__icon" />
+              <p className="footer__text">Instargram</p>
+            </div>
+            <div className="footer__box">
+              <Image src={img4} alt="logo" className="footer__icon" />
+              <p className="footer__text">Tik Tok</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default memo(Footer);
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,49 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import logo from "@/assets/images/logo.png";
+import "./nav.css";
 
 const Nav = () => {
   return (
-    <div>
-      <h2>nav</h2>
-      <Link href={"/"}>Home</Link>
-      <Link href={"/about"}>About</Link>
-      <Link href={"/contact"}>Contact</Link>
-    </div>
+    <nav className="nav">
+      <div className="container">
+        <div className="nav__info">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <Link href={"/"} className="nav__link">
+                Home
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link href={"/shop"} className="nav__link">
+                Shop
+              </Link>
+            </li>
+            <li className="nav__item">
+              <Link href={"/blog"} className="nav__link">
+                Blog
+              </Link>
+            </li>
+          </ul>
+          <Image src={logo} alt="logo" className="nav__img" />
+          <ul className="nav__list">
+            <li className="nav__item">
+              <Link href={"/about"} className="nav__link">
+                About Us
+              </Link>
+            </li>
+            <div className="nav__box">
+              <input
+                placeholder="Search Product"
+                type="text"
+                className="nav__input"
+              />
+            </div>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
